995db872b30d31c36714f1ab0f11ec42f70b0ead
Ciscoo91 <franciscomambo91@gmail.com>
2020-09-10 03:27:22 +0200

    feat: added basic setup for easyadmin backend

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\MovieList;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        // return parent::index();
+        return $this->render('bundles/EasyAdminBundle/welcome.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('My Movie List');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Movie Lists', 'fa fa-film', MovieList::class);
+    }
+}
